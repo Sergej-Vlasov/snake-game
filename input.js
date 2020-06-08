1e43e7da02578a21b1598d0c137931040fddf356
Sergej Vlasov <sergej.vlasov@neuralalpha.com>
2020-06-08 22:09:29 +0100

    adjusted input direction to prevent reversing

--- input.js
+++ input.js
@@ -1,23 +1,29 @@
 let inputDirection = { x: 0, y: 0 };
+let lastInputDirection = { x: 0, y: 0 };
 
 window.addEventListener("keydown", e => {
   // up: y = -1; down: y = 1; left: x = -1; right: x = 1
   switch (e.key) {
     case "ArrowUp":
+      if (lastInputDirection.y !== 0) break; // this prevents from reversing on the y axis
       inputDirection = { x: 0, y: -1 };
       break;
     case "ArrowDown":
+      if (lastInputDirection.y !== 0) break;
       inputDirection = { x: 0, y: 1 };
       break;
     case "ArrowLeft":
+      if (lastInputDirection.x !== 0) break; // this prevents from reversing on the x axis
       inputDirection = { x: -1, y: 0 };
       break;
     case "ArrowRight":
+      if (lastInputDirection.x !== 0) break;
       inputDirection = { x: 1, y: 0 };
       break;
   }
 });
 
 export function getInputDirection() {
+  lastInputDirection = inputDirection;
   return inputDirection;
 }
